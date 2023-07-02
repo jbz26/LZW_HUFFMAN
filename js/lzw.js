8bec0a48c0fed74f075dfa6749bbf005f7e55ba4
Thien Bao <94024704+jbz26@users.noreply.github.com>
2023-07-02 14:04:41 +0700

    Update lzw.js

--- js/lzw.js
+++ js/lzw.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.L_encoding=exports.L_decoding= void 0;
+exports.L_encoding=exports.L_decoding= exports.compress_ratio_LZW=void 0;
 function L_encoding(text){
     var next=[],current=[],output=[],dictionary={};
     var code=256;
@@ -32,8 +32,6 @@ function L_encoding(text){
             }
         }
     }
-    //current.push(last);
-    //output.push(last.charCodeAt(0));
 
     return {
         current,
@@ -43,26 +41,68 @@ function L_encoding(text){
     }
 }
 exports.L_encoding=L_encoding
-function L_decoding(codes,text){
+function L_decoding(text){
     let t=0;
     let out="";
-    let dict=Object.keys(codes);
-    let val=Object.values(codes);
-    for (let i=0; i<text.length;i++){
+    var dict={};
+    var last;
+    var code=256;
+    for (let i=0;i<text.length;i++){
         t=t*10+parseInt(text[i],10);
-        if(t<32){
-        }
+        console.log(t," " ,last);
+        if(t<32){}
         else if(t<256){
-            out+=String.fromCharCode(t);
+            var c=String.fromCharCode(t);
+            if(last!=undefined){
+                dict[code++]=last+c;
+            }
+            last = c;
+            out+=last;
             t=0;
-        }
-        else{
-            if(val.includes(t)){
-                out+=String(dict[val.indexOf(t)]);
-                t=0;
+        }else{
+            if(last==undefined){}
+            else{
+                if(dict[t]==undefined){
+                    let cur=last+last[0];
+                    last=cur;
+                    out+=cur;
+                    dict[t]=cur;
+                    t=0;
+                }else{
+                    let cur=dict[t];
+                    out+=cur;
+                    dict[code++]=last+cur[0];
+                    last=cur;
+                    t=0;
+                }
             }
         }
     }
+
     return out;
 }
 exports.L_decoding=L_decoding;
+function compress_ratio_LZW(text,codes){
+    let before=text.length*8;
+    let after=0;
+    for (let i=0;i<codes.output.length;i++){
+        let t=codes.output[i];
+        console.log(t);
+        if(t<256){
+            after+=8;
+        }
+        else
+            after+=t.toString(2).length;
+        
+        console.log(t.toString(2).length);
+    }
+
+    let CR=(before)/after;
+    let content='Length of input string: ' + text.length
+            +'<br> Total bits: '+ before
+            +'<br> Total bits after compress: ' + after 
+            +'<br> Compress ratio: '+ CR.toFixed(2)
+            +'<br> Space Savings: '+ ((1-1/CR)*100).toFixed(2) + "%";
+return content;
+}
+exports.compress_ratio_LZW=compress_ratio_LZW;
